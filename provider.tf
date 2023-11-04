a07df255865cad06b451bc5ba5f9121c296c1440
mrschmaltz <vschmaltz@outlook.com>
2023-11-04 19:51:07 +0000

    acocunt

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "mystorageaccount"
+  name                     = "wcmcmystorageaccount"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
